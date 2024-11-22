de66cdc6c88101ad383c750854674a22e7fdc3db
tomQuintana <tomas.quintana@gojiraf.com>
2024-11-22 17:46:07 -0300

    🎨 Add interface for pie chart and pass a data by props

--- app/lifetracker/expenses/totalByType/page.tsx
+++ app/lifetracker/expenses/totalByType/page.tsx
@@ -3,24 +3,39 @@
 import React, { useEffect, useState } from "react";
 import PieChart from "@/components/ui/charts/PieChart";
 import { getExpensesByType } from "@/services/expenses";
+import { useSession } from "next-auth/react";
+
 
 const totalByType = () => {
 
+return (
+    <div className="font-mono flex flex-row justify-center items-start gap-8">
+      {/* Gráfico */}
+      <div className="lex-1 text-center rounded-3xl shadow">
+        <h1 className="text-3xl mb-4">Total By Type - Pie Chart</h1>
+        <PieChart
+          dataExpenses={{
+            expenses: {
+              food: 4000,
+              goOut: 10,
+              other: 5000,
+              fixed: 1000,
+              books: 2000,
+              fubol: 100,
+              montly: 1000,
+              target: 1000,
+            }
+          }}
+        />
+      </div>
 
-  return (
-    <div className="font-mono justify-center text-center">
-      <div>
-        <h1 className="text-3xl">Total By Type - Pie Chart</h1>
-      </div>
-      <div>
-        <PieChart />
-      </div>
-      <div className="text-2xl rounded-xl shadow">
-        <h2 className="bg-orange-300 text-white rounded-lg">Total Expend</h2>
-        <div>
+      {/* TotalCard */}
+      <div className="text-2xl rounded-xl shadow flex-1 text-center p-4 mt-16">
+        <h2 className="bg-orange-300 text-white rounded-lg p-2">Total Expend</h2>
+        <div className="mt-4">
           <span>ARS: 1000</span>
         </div>
-        <div>
+        <div className="mt-2">
           <span>USDT: 1000</span>
         </div>
       </div>
--- components/ui/charts/PieChart.tsx
+++ components/ui/charts/PieChart.tsx
@@ -1,83 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import { Pie } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { getExpensesByType } from '@/services/expenses';
+import { useState, useEffect } from "react";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
-// Registrar los elementos necesarios
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-// Función mock para obtener los gastos
-const mockGetExpenses = async () => {
-  return [
-    { type: 'Food', price_ARS: 1200 },
-    { type: 'Transport', price_ARS: 800 },
-    { type: 'Entertainment', price_ARS: 500 },
-    { type: 'Utilities', price_ARS: 1000 },
-    { type: 'Health', price_ARS: 700 },
-  ];
-};
+interface Expenses {
+  food: number;
+  goOut: number;
+  other: number;
+  fixed: number;
+  books: number;
+  fubol: number;
+  montly: number;
+  target: number;
+}
 
-const PieChart = () => {
-  const [expenses, setExpenses] = useState([]);
-  const [expenseByType, setExpensesByType] = useState([]);
-  const [totalByType, setTotalByType] = useState([]);
+interface ExpensesTypeProps {
+  dataExpenses: {
+    expenses: Expenses;
+  };
+}
 
-  useEffect(() => {
-    const fetchExpenses = async () => {
-      try {
-        // Utiliza la función mock en lugar de la llamada real a la API
-        const response = await mockGetExpenses();
-        const data = await getExpensesByType();
-        console.log(data);
-        
-        setExpensesByType(data.expenses);
-        setTotalByType(data.total);
-        setExpenses(response);
-      } catch (error) {
-        console.error('Error fetching expenses:', error);
-      }
-    };
+const PieChart: React.FC<ExpensesTypeProps> = ({ dataExpenses }) => {
 
-    fetchExpenses();
-  }, []);
-  
-
-  // Crear las etiquetas y los valores para el gráfico a partir de los datos de gastos
-  const labels = expenses.map(expense => expense.type);
-  const dataValues = expenses.map(expense => expense.price_ARS);
+  const expenseEntries = Object.entries(dataExpenses.expenses);
+  const labels = expenseEntries.map(([type, _]) => type); 
+  const dataValues = expenseEntries.map(([_, price_ARS]) => price_ARS); 
 
   const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: 'Gastos en ARS',
-        data: dataValues,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
+  labels,
+  datasets: [
+    {
+      label: "Gastos en ARS",
+      data: dataValues,
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)", 
+        "rgba(255, 99, 71, 0.2)",  
+        "rgba(0, 255, 0, 0.2)", 
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",  
+        "rgba(255, 99, 71, 1)", 
+       "rgba(0, 255, 0, 1)", 
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 
   const options = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top',
+        position: "top",
       },
       tooltip: {
         enabled: true,
@@ -86,7 +71,7 @@ const PieChart = () => {
   };
 
   return (
-    <div className="" style={{ height: '620px', width: '600px' }}>
+    <div className="flex justify-center items-center" style={{ height: "620px", width: "600px" }}>
       <Pie data={data} options={options} />
     </div>
   );
